fa18fb60e2e9adb276ddbeb7d969ef126577cf49
Ryan <rylarson17@gmail.com>
2022-11-29 00:44:25 -0700

    Added get requests for main page and notes

--- server.js
+++ server.js
@@ -1,2 +1,27 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+const notes = require("./db/db.json");
+
+const PORT = 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+app.get("/api/notes", (req, res) => {
+  res.status(200).json(notes);
+});
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
